28be3cbde4d44b846cb7b6b55123ce7b6a5a2355
Felipe Cardozo <pypelyncar@hotmail.com>
2017-03-16 23:46:51 +0100

    nav pushin new month from resume

--- src/pages/resume/resume.ts
+++ src/pages/resume/resume.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FinancialService } from '../../app/services/financial.service';
 import { NavController } from 'ionic-angular';
+import { MonthsPage } from '../months/months';
 
 @Component({
   selector: 'resume',
@@ -8,8 +9,21 @@ import { NavController } from 'ionic-angular';
 })
 export class ResumePage {
 
-  constructor(public navCtrl: NavController, private financialService:FinancialService) {
+  @ViewChild('myNav') nav: NavController;
+  month:String;
+  rootPage: any = ResumePage;
 
+  constructor(public navCtrl: NavController, private financialService:FinancialService) {
+    
+  }
+
+  pushPage(){
+    // push another page on to the navigation stack
+    // causing the nav controller to transition to the new page
+    // optional data can also be passed to the pushed page.
+    this.navCtrl.push(MonthsPage, {
+      month: this.financialService.currentMonth
+    });
   }
 
 }
